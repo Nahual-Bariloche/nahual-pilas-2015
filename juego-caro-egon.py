eb85b08138c2a94f408dde34ed0899627c847fbe
Pedro Wood <mail_pedro@yahoo.com.ar>
2015-10-21 09:01:22 -0300

    mejoras de Jere y Caro clases 13 y 20 de Oct

--- juego-caro-egon.py
+++ juego-caro-egon.py
@@ -2,8 +2,26 @@
 import pilasengine
 
 pilas = pilasengine.iniciar()
-pilas.fondos.Volley()
-puntaje = pilas.actores.Puntaje(-280, 200, color=pilas.colores.violeta)
+
+pilas.fondos.Tarde()
+
+def iniciar_juego():
+    juego=Juego()
+    juego.iniciar()
+def salir_del_juego():
+    pilas.terminar()
+    
+menu=pilas.actores.Menu(
+        [
+            ('iniciar juego', iniciar_juego),
+            ('salir', salir_del_juego),
+        ])
+
+
+
+
+    
+   
 class Rzck(pilasengine.actores.Actor):
     def iniciar(self):
      
@@ -11,19 +29,33 @@ class Rzck(pilasengine.actores.Actor):
         self.x= 0
         self.y= -150
         
-rzck = Rzck(pilas)
+class Juego():
+    puntaje = pilas.actores.Puntaje(-280, 200, color=pilas.colores.violeta)    
 
-rzck.aprender("arrastrable")
-
-
-
-bomba= pilas.actores.Bomba() *100
-
-def cuando_colisiona(rzck, bomba):
+    def iniciar(self):
+        menu.eliminar()
+        pilas.fondos.Volley()
+        rzck = Rzck(pilas)
+        rzck.aprender("arrastrable")
+        bomba= pilas.actores.Bomba() *10
+        pilas.colisiones.agregar(rzck, bomba,self.cuando_colisiona)
+        aceitunas=pilas.actores.Aceituna()*10
+        pilas.colisiones.agregar(rzck, aceitunas,self.cuando_colisiona1)
+        una_tarea = pilas.tareas.siempre(5, self.agregar_aceituna,aceitunas)
+        
+    def agregar_aceituna(self, aceitunas):
+        aceituna=pilas.actores.Aceituna()*2
+        aceitunas.agregar(aceituna)
     
-    bomba.explotar()
-    puntaje.aumentar (10)
-pilas.colisiones.agregar(rzck, bomba, cuando_colisiona)
-
-
+    def cuando_colisiona(self, rzck, bomba):
+        bomba.explotar()
+        self.puntaje.aumentar (10)
+        
+    def cuando_colisiona1(self, rzck, banana):
+        banana.eliminar()
+        self.puntaje.reducir(10)
+        if (self.puntaje.texto=="0"):
+            rzck.eliminar()
+            texto = pilas.actores.Texto ("juego terminado")
+            pilas.fondos.Noche()                                                                                  
 pilas.ejecutar()
