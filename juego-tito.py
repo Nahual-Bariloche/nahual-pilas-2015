6eb5733e0a3c5b89958859a01fd01f0a54766459
Pedro Wood <mail_pedro@yahoo.com.ar>
2015-10-21 08:57:28 -0300

    mejoras clase 13/10

--- juego-tito.py
+++ juego-tito.py
@@ -2,39 +2,59 @@
 import pilasengine
 
 pilas = pilasengine.iniciar()
+
+pilas.fondos.Selva()
+
+def iniciar_juego():
+    juego=Juego()
+    juego.iniciar()
+#    juego.agrega()
+    menu.eliminar()
+
+def salir_del_juego():
+    pilas.terminar()
+
+menu=pilas.actores.Menu(
+        [
+            ('iniciar juego', iniciar_juego),
+            ('salir', salir_del_juego),
+        ])
+
 class Pepa (pilasengine.actores.Actor):
     def iniciar(self):
         self.imagen=self.pilas.imagenes.cargar_grilla("pingu.png", 10)
         self.x=300
         self.y=205
-        
+
     def mover_a_la_posicion_del_mouse(self, evento):
         self.x=evento.x
         self.y=evento.y
-            
-def comer_banana(pepa,banana):
-    banana.eliminar()
-    puntaje.aumentar()
 
-def explotar_bomba(pepa,bomba):
-    bomba.explotar()
-    puntaje.reducir(5)
-    
-pilas.fondos.Tarde()  
-        
-pepa=Pepa(pilas)
+class Juego():
 
-pilas.eventos.mueve_mouse.conectar(pepa.mover_a_la_posicion_del_mouse)
+    puntaje=pilas.actores.Puntaje(-250,220,color=pilas.colores.blanco)
 
-banana=pilas.actores.Banana()*15
+    def comer_banana(self,pepa,banana):
+        banana.eliminar()
+        self.puntaje.aumentar(1)
 
-bomba=pilas.actores.Bomba()*5
+    def explotar_bomba(self,pepa,bomba):
+        bomba.explotar()
+        self.puntaje.reducir(5)
 
-pilas.colisiones.agregar(pepa,banana,comer_banana)
+    def iniciar(self):
+        pilas.fondos.Tarde()
+        pepa=Pepa(pilas)
+        pilas.eventos.mueve_mouse.conectar(pepa.mover_a_la_posicion_del_mouse)
+        banana=pilas.actores.Banana()*30
+        bomba=pilas.actores.Bomba()*5
+        pilas.colisiones.agregar(pepa,banana,self.comer_banana)
+        pilas.colisiones.agregar(pepa,bomba,self.explotar_bomba)
 
-pilas.colisiones.agregar(pepa,bomba,explotar_bomba)
 
-puntaje=pilas.actores.Puntaje(-250,220,color=pilas.colores.blanco)
-
+    def agregar_bombas(self):
+        bomba.agregar(pilas.actores.Bomba(x=50,y=60))
+    def agrega(self):
+        pilas.tareas.siempre(2,agregar_bombas)
 
 pilas.ejecutar()
