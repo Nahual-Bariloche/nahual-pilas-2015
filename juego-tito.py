b387302231c20483e13bc570f35135e6e441add1
Pedro Wood <mail_pedro@yahoo.com.ar>
2015-10-21 08:58:05 -0300

    mejoras clase 20/10

--- juego-tito.py
+++ juego-tito.py
@@ -8,9 +8,10 @@ pilas.fondos.Selva()
 def iniciar_juego():
     juego=Juego()
     juego.iniciar()
-#    juego.agrega()
     menu.eliminar()
-
+    juego.agrega()
+    
+    
 def salir_del_juego():
     pilas.terminar()
 
@@ -25,36 +26,46 @@ class Pepa (pilasengine.actores.Actor):
         self.imagen=self.pilas.imagenes.cargar_grilla("pingu.png", 10)
         self.x=300
         self.y=205
-
+        
     def mover_a_la_posicion_del_mouse(self, evento):
         self.x=evento.x
         self.y=evento.y
-
+        
 class Juego():
-
+    
     puntaje=pilas.actores.Puntaje(-250,220,color=pilas.colores.blanco)
-
+    
     def comer_banana(self,pepa,banana):
         banana.eliminar()
-        self.puntaje.aumentar(1)
-
+        self.puntaje.aumentar(10)
+    
     def explotar_bomba(self,pepa,bomba):
         bomba.explotar()
-        self.puntaje.reducir(5)
+        self.puntaje.reducir(10)
+        if (self.puntaje.texto == "0"):
+            texto=pilas.actores.Texto("hola")
+            pilas.fondos.Blanco()
+            pepa.eliminar()
 
     def iniciar(self):
-        pilas.fondos.Tarde()
+        pilas.fondos.Tarde()  
         pepa=Pepa(pilas)
         pilas.eventos.mueve_mouse.conectar(pepa.mover_a_la_posicion_del_mouse)
         banana=pilas.actores.Banana()*30
-        bomba=pilas.actores.Bomba()*5
+        self.bomba=pilas.actores.Bomba()*5
         pilas.colisiones.agregar(pepa,banana,self.comer_banana)
-        pilas.colisiones.agregar(pepa,bomba,self.explotar_bomba)
+        pilas.colisiones.agregar(pepa,self.bomba,self.explotar_bomba)
 
 
     def agregar_bombas(self):
-        bomba.agregar(pilas.actores.Bomba(x=50,y=60))
+        
+        nueva_bomba=pilas.actores.Bomba() 
+        nueva_bomba.x =  pilas.azar(-200, 200) 
+        nueva_bomba.y =  pilas.azar(-200, 200)           
+        self.bomba.agregar(nueva_bomba)
+
     def agrega(self):
-        pilas.tareas.siempre(2,agregar_bombas)
+        pilas.tareas.siempre(2,self.agregar_bombas)
+    
 
 pilas.ejecutar()
